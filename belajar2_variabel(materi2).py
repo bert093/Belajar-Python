57303ca62901a8872815f9b0133896a095f04939
bert093 <sas992203@gmail.com>
2024-08-01 16:13:45 +0800

    Update belajar2_variabel(materi2).py

--- belajar2_variabel(materi2).py
+++ belajar2_variabel(materi2).py
@@ -1,4 +1,8 @@
 nama = "Syahril" # *contoh string
-Umur = "18" # * contoh integer
+umur = "18" # * contoh integer
 tinggi = "1,75" # * contoh desimal
-saya_mahasiswa = true # * contoh boolean
+angka = 23.7882123
+# saya_mahasiswa = true
+# * f adalah string atau formatted string literal. Ini adalah cara untuk menyisipkan nilai variabel ke dalam string dengan mudah.
+print(f"Halo!, nama saya {nama} saya berumur {umur} dan tinggi saya {tinggi}")
+print(f"angka = {angka.2f}")
