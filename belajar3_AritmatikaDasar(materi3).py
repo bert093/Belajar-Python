73f2e0e12d8252efe454a8459a1ec47f5784a449
bert093 <sas992203@gmail.com>
2024-08-01 16:43:12 +0800

    membuat file baru materi 3
    Pembelajaran kali ini membahas tentang aritmatika dasar dan operator logika

---
+++ belajar3_AritmatikaDasar(materi3).py
@@ -0,0 +1,26 @@
+a = 12
+b = 9
+hasil = a + b # pertambahan
+hasil2 = a - b # pengurangan
+hasil3 = a * b # perkalian
+hasil4 = a / b # pembagian 
+hasil5 = a // b # menghasilkan hasil pembagian dalam bilangan bulat
+hasil6 = a % b # hasil sisa pembagian
+hasil7 = a ** b
+print(hasil)
+print(hasil2)
+print(hasil3)
+print(hasil4)
+print(hasil5)
+print(hasil6)
+print(hasil7)
+
+sama_dengan = (a == b) # memeriksa dua nilai apakah sama atau tidak
+tidak_samadengan = (a != b) # memeriksa dua nilai apakah a tidak sama dengan b
+lebih_besar = (a > b) # memeriksa dua nilai apakah a lebih besar daripada b
+apakah = (hasil >= 18 and hasil<= 65)
+
+print(sama_dengan)
+print(tidak_samadengan)
+print(lebih_besar)
+print(apakah)
