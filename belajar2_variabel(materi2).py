f52ce7c3f67703a6b92c41e8a996134d60eafa12
bert093 <sas992203@gmail.com>
2024-08-01 16:23:00 +0800

    fixed

--- belajar2_variabel(materi2).py
+++ belajar2_variabel(materi2).py
@@ -1,8 +1,9 @@
 nama = "Syahril"  # *contoh string
 umur = 18         # * contoh integer
-tinggi = 1.75     # * contoh desimal
+tinggi = 1.75     # * contoh float/desimal
 angka = 23.7882123
 # saya_mahasiswa = true
 # f adalah string atau formatted string literal. Ini adalah cara untuk menyisipkan nilai variabel ke dalam string dengan mudah.
 print(f"Halo!, nama saya {nama} saya berumur {umur} dan tinggi saya {tinggi}")
-print(f"angka = {angka:.3f}") # digunakan untuk membatasi jumlah angka di belakang koma menjadi dua angka desimal.
+print(f"angka = {angka:.3f}") # digunakan untuk membatasi jumlah angka di belakang koma menjadi tiga angka desimal.
+print(f"angka= {angka}") # mencetak semua angka desimalnya.
